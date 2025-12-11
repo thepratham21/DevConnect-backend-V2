df6ff7b04f60018ddfed17e61cb7591079ee7ff0
Prathmesh Shinde <prathmeshshinde021@gmail.com>
2025-12-11 12:02:56 +0530

    -

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
     origin: [
     "http://localhost:5173",
-    "https://your-frontend-domain.onrender.com"
+    "https://devconnect-backend-v2.onrender.com"
 ],
     credentials: true,
 
